c9833aeca56e67a10fe49bb41400dd86f3a9ca19
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-01-18 23:07:14 -0600

    setting up canvas

---
+++ main.js
@@ -0,0 +1,10 @@
+class Enemy {}
+
+class Game {}
+
+window.addEventListener("load", function () {
+  const canvas = this.document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
