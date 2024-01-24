714b5b6b66c76fd43d2facf92150f330ea7504c5
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-01-23 22:25:18 -0600

    working on enemy class

--- enemy.js
+++ enemy.js
@@ -1,10 +1,10 @@
 class Enemy {
   constructor(game) {
     this.game = game;
-    this.x = 150;
-    this.y = 200;
+    this.x = Math.random() * this.game.width;
+    this.y = 0;
     this.speedX = 0;
-    this.speedY = 1;
+    this.speedY = 10;
     this.width = 50;
     this.height = 50;
   }
@@ -12,7 +12,13 @@ class Enemy {
   update() {
     this.x += this.speedX;
     this.y += this.speedY;
+
+    if (this.y > this.game.height) {
+      this.x = Math.random() * this.game.width;
+      this.y = 0;
+    }
   }
+
   draw() {
     this.game.ctx.fillStyle = "red";
     this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
