cae1dc0414746373b29198c6105b93d97db90790
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-01-23 22:19:55 -0600

    working on enemy class

--- enemy.js
+++ enemy.js
@@ -1,12 +1,15 @@
 class Enemy {
   constructor(game) {
     this.game = game;
-    this.x;
-    this.y;
+    this.x = 150;
+    this.y = 200;
     this.speedX;
     this.speedY;
     this.width = 50;
     this.height = 50;
   }
-  draw() {}
+  draw() {
+    this.game.ctx.fillStyle = "red";
+    this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
 }
