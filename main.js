b3a224b89893ec878c8cf3d767ae0a1abb3b41b9
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-01-28 23:00:15 -0600

    working on collision

--- main.js
+++ main.js
@@ -42,6 +42,15 @@ class Game {
     this.ctx.fillStyle = "blue";
   }
 
+  checkCollision(rect1, rect2) {
+    return (
+      rect1.x < rect2.x + rect2.width &&
+      rect1.x + rect1.width > rect2.x &&
+      rect1.y < rect2.y + rect2.height &&
+      rect1.y + rect1.height > rect2.y
+    );
+  }
+
   createEnemyPool() {
     for (let i = 0; i < this.numberOfEnemies; i++) {
       this.enemyPool.push(new Enemy(this));
