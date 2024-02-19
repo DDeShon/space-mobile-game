8824a41b46eaa8e5702a7dd4ae5104b38b09ac56
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-02-18 21:44:52 -0600

    working on enemy class

--- enemy.js
+++ enemy.js
@@ -102,6 +102,7 @@ class Beetlemorph extends Enemy {
   constructor(game) {
     super(game);
     this.image = document.getElementById("beetlemorph");
+    this.lastFrame = 3;
   }
 
   start() {
@@ -109,7 +110,30 @@ class Beetlemorph extends Enemy {
     this.speedX = 0;
     this.speedY = Math.random() * 2 + 0.2;
     this.lives = 1;
-    this.lastFrame = 3;
+  }
+
+  update() {
+    super.update();
+    if (!this.free) {
+      if (this.isAlive()) {
+        this.hit();
+      }
+    }
+  }
+}
+
+class lobstermorph extends Enemy {
+  constructor(game) {
+    super(game);
+    this.image = document.getElementById("lobstermorph");
+    this.lastFrame = 14;
+  }
+
+  start() {
+    super.start();
+    this.speedX = 0;
+    this.speedY = Math.random() * 0.5 + 0.2;
+    this.lives = 1;
   }
 
   update() {
