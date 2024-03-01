10c10edf6eeaaa2ff0b1558bfcae7c119411d78c
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-02-29 19:19:38 -0600

    working on game audio

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ class AudioControl {
   constructor() {
     this.newgame = document.getElementById("newgame");
     this.boom1 = document.getElementById("boom1");
+    this.boom2 = document.getElementById("boom2");
+    this.boom3 = document.getElementById("boom3");
+    this.boom4 = document.getElementById("boom4");
     this.slide = document.getElementById("slide");
   }
 
