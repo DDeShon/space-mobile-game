77de8448a828d006eb4bbf44c8357a4f12016f4e
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-03-03 23:01:22 -0600

    working on game audio

--- enemy.js
+++ enemy.js
@@ -281,7 +281,9 @@ class Imploding extends EnemyState {
     this.enemy.minFrame = 6;
     this.enemy.maxFrame = this.enemy.lastFrame + 1;
     this.enemy.frameX = this.enemy.minFrame;
-    this.game.sound.play(this.game.sound.boom1);
+    this.game.sound.play(
+      this.game.sound.boomSounds[Math.floor(Math.random() * 4)]
+    );
   }
 
   update() {}
