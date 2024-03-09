335b39f049af5087e492f5d999dd3d40cef9e21b
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-03-08 23:26:54 -0600

    working on enemy class

--- main.js
+++ main.js
@@ -168,13 +168,15 @@ class Game {
   createEnemyPool() {
     for (let i = 0; i < this.numberOfEnemies; i++) {
       const randomNumber = Math.random();
-      if (randomNumber < 0.8) {
+      const score = 0;
+      if (randomNumber < 0.8 && score < 20) {
         this.enemyPool.push(new Beetlemorph(this));
-      } else {
+      } else if (randomNumber > 0.8 && score < 20) {
         this.enemyPool.push(new Lobstermorph(this));
+      } else if (score > 20) {
+        this.enemyPool.push(new Phantommorph(this));
       }
     }
-    // this.enemyPool.push(new Phantommorph(this));
   }
 
   getEnemy() {
